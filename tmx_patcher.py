29e39009ea4de6013fe554aa2d79274dcbb82174
pankajksharma <pankaj@pankajksharma.com>
2014-08-01 22:26:52 +0530

    updated tmx patcher

--- tmx_patcher.py
+++ tmx_patcher.py
@@ -1,7 +1,10 @@
 import os.path
 import argparse, re, sys
+from lib.fms import FMS
 from lib.ap import Apertium
 from lib.tmxfile import TMXFile
+from lib.patcher import Patcher
+from lib.utilities import preprocess, assertion
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -14,6 +17,7 @@ parser.add_argument('LP', help='Language Pair for TM (for example en-eo)')
 parser.add_argument('-v', help='Verbose Mode', action='store_true')
 parser.add_argument('-t', help='Show patching traces', action='store_true')
 parser.add_argument('-d', help='Specify the lanuguage-pair installation directory')
+parser.add_argument('--cam', help='Only those patches which cover all the mismatches', action='store_true')
 parser.add_argument('--min-fms', help='Minimum value of fuzzy match score of S and S1.', default='0.8')
 parser.add_argument('--min-len', help='Minimum length of sub-segment allowed.', default='2')
 parser.add_argument('--max-len', help='Maximum length of sub-segment allowed.', default='5')
@@ -35,6 +39,7 @@ assertion(len(lps) == 2, "LP should be of type a-b, eg, 'en-eo'")
 lp_dir = args.d
 verbose = args.v
 show_traces = args.t
+cover_all = args.cam
 min_fms = float(args.min_fms)
 min_len = int(args.min_len)
 max_len = int(args.max_len)
@@ -44,7 +49,7 @@ apertium = Apertium(lps[0], lps[1])
 (out, err) = apertium.check_installations(lp_dir)
 assertion(out, err)
 
-tmxf = TMXFile(tmname, pairs[0], pairs[1])
+tmxf = TMXFile(tmxfile, lps[0], lps[1])
 tmunits = tmxf.getunits()
 
 fmses = {}
@@ -63,8 +68,8 @@ assertion(fmses != {}, "No proper match with FMS > {0} could be found".format(mi
 sorted_fms = sorted(fmses, key=fmses.get)
 
 (src, tgt) = sorted_fms[0] #Best match 
-
-patches = Patcher(apertium, s_sentence, s1_sentence, t_sentence).patch(min_len, max_len)
+# print(s_sentence, src, tgt)
+patches = Patcher(apertium, s_sentence, src, tgt).patch(min_len, max_len)
 # print patches
 for (patch, features, _, _, _, cam, traces) in patches:
 	if cover_all and cam:
